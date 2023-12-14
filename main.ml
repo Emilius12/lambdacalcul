b0b24389da80e9d27bf4117ad4038cab583cc8b4
Emile Fourcin <emile.fourcin1@gmail.com>
2023-12-14 22:48:46 +0100

    conversion variable terme

--- main.ml
+++ main.ml
@@ -2,7 +2,7 @@ type variable = Var of char;;
 type terme = V of variable | App of terme*terme | Lamb of variable*terme;;
 
 
+let var (x: terme) =  match x with  (* permet de passer d'un terme à la variable correspondante*)
+V(c) -> c;
+|_ -> failwith "pas une variable";;
 
-
-let x = V(Var('x'));;
-x;;
